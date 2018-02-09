0134559dbfe8de4ccb29043c9f3b8f93144a6920
Abel Gancsos <35180721+agancsos2@users.noreply.github.com>
2018-02-08 21:50:44 -0500

    Update drop_all.php

--- FriendlyPrint/admin/drop_all.php
+++ FriendlyPrint/admin/drop_all.php
@@ -5,10 +5,10 @@
 	require_once("../bin/connect.php");
 	require_once("../bin/functions.php");
 
-	$table_query = "select table_name from information_schema.tables where table_schema = '$__DB__DATABASE'";
+	$table_query = "select table_name from information_schema.tables where table_schema = '$DB__CONNECT'";
 	$getter = mysql_query($table_query);
 	while($table_name = mysql_fetch_assoc($getter))
 	{
-		mysql_query("drop table $__DB__DATABASE." . $table_name['table_name']);
+		mysql_query("drop table $DB__CONNECT." . $table_name['table_name']);
 	}
 ?>
