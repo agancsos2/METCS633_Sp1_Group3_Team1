15f46350bbf32ba133b028f78fe56f82623927ec
Abel Gancsos <35180721+agancsos2@users.noreply.github.com>
2018-02-11 00:18:04 -0500

    Update aboutus.php

--- FriendlyPrint/bin/aboutus.php
+++ FriendlyPrint/bin/aboutus.php
@@ -3,7 +3,6 @@
  require_once("header.php");
  require_once("functions_db.php");
 ?>
-<center>
  Innovative Print Shop, IPS, is an Ohio based print shop company that seeks to
 gain a larger share in their business market. Specifically, IPS wishes to build a
 customer base in the neighboring states of Indiana and Kentucky. Currently,
@@ -14,7 +13,6 @@ simply ramp up shop space and resources in Ohio, and obviate their need to
 have a physical presence in their target customer locations. Therefore, the IPS
 business development team has contracted our software group to develop a
 technical solution to facilitate their business expansion effort.
-</center>
 <?php
  require_once("footer.php");
 ?>
